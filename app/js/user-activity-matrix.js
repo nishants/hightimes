cbfef0fd498ef0cf34cca65b629248a4d6aa2ac6
nishant <nishant.singh87@gmail.com>
2015-05-06 02:17:06 +0530

    creating user activity matrix.

---
+++ app/js/user-activity-matrix.js
@@ -0,0 +1,27 @@
+(function () {
+  "user strict"
+
+  var dayOfWeekOf = function(unixTime){
+    return new Date(1000 * unixTime).getDay();
+  };
+  var hoursOfDayOf = function(){
+    return new Date(1000 * unixTime).getHours();
+  };
+
+  var UserActivityMatrix = function (periodLengthInHours) {
+    this.intervalInHours = periodLengthInHours;
+    this.matrix = [];
+  };
+
+  UserActivityMatrix.prototype.addActivityAt = function (unixTime) {
+    var dayOfWeek = dayOfWeekOf(unixTime);
+    var period    = hoursOfDayOf(unixTime)/this.intervalInHours;
+    return this.matrix[dayOfWeek][period];
+  };
+
+  UserActivityMatrix.prototype.activityAt = function (dayOfWeek, period) {
+    return this.matrix[dayOfWeek][period];
+  };
+
+  hightimes.UserActivityMatrix = UserActivityMatrix;
+}).call(this);
