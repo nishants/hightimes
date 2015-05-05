7063160857d54736efe70c99f53fb98c584ef921
nishant <nishant.singh87@gmail.com>
2015-05-06 02:59:22 +0530

    extracting out loginpage.js

---
+++ app/js/views/login-page.js
@@ -0,0 +1,59 @@
+(function () {
+  "use strict"
+  var LoginPage = function($page, onLogin){
+
+    var toUserNames = function(users){
+      var usernames = [];
+      for(var i =0 ; i< users.length; i++){
+        usernames.push(users[i].username)
+      }
+      return usernames;
+    };
+
+    var invalidUserId = function () {
+      alert("Please enter a valid user id.");
+    };
+
+    var showModal = function (data) {
+      var onSelect = function (index) {alert("selected : "+ data[index]);}
+      hightimes.Modal.create($("#select-user-dialog"), data).show(onSelect);
+    };
+
+    var userID = function () {
+      return $("#user-id").val();
+    };
+    var username = function () {
+      return $("#username").val();
+    };
+
+    var destroy = function(){
+      $page.hide();
+    };
+
+    $("#login-by-id").mousedown(function(e){
+      hightimes.findUserById(userID()).ok(function(user){
+        onLogin(user);
+        destroy();
+      }).error(invalidUserId);
+    });
+
+    $("#login-by-name").mousedown(function(e){
+      hightimes.searchUsername(username()).ok(function (user) {
+        console.log(user.full_name);
+      }).error(function(users){
+        showModal(toUserNames(users));
+      });
+    });
+
+    this.show = function(){
+      $page.show();
+      return this;
+    };
+
+  };
+
+  hightimes.showLoginPage = function($page, onLogin){
+    new LoginPage($page, onLogin).show();
+  };
+
+}).call(this);
