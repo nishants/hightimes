f111ba02f45d553e31f5e8ef988b3f2bc290a5d1
nishant <nishant.singh87@gmail.com>
2015-05-06 13:19:59 +0530

    extracting page.js and pagedresource.js

--- app/js/hightimes/paged-resource.js
+++ app/js/hightimes/paged-resource.js
@@ -1,6 +1,8 @@
 (function(){
   "use strict"
 
+  var Page = hightimes.Page;
+
   var PagedResource = function(url){
     this.url = url;
   };
@@ -38,5 +40,32 @@
     load(this.url, fetchPage, callback);
   };
 
-  hightimes.PagedResource = PagedResource;
+  var forAllPostsOfFollowersDo = function (userID, forEachFollowersDo, forEachFollowerPostDo) {
+    var url = "https://api.instagram.com/v1/users/<user-id>/followed-by".replace("<user-id>", userID);
+    var pageCount = 1;
+
+    new PagedResource(url).forEach(function (page) {
+      console.log("Followers on page-" + (pageCount++) + " : ")
+      var followers = page.dataList();
+      for (var i = 0; i < followers.length; i++) {
+        var follower = followers[i];
+        forEachFollowersDo(follower);
+
+        var userPostsUrl = "https://api.instagram.com/v1/users/<user-id>/media/recent".replace("<user-id>", follower.id);
+        new PagedResource(userPostsUrl).forEach(function (page) {
+          if (page.dataList()) {
+            var posts = page.dataList();
+            for (var i = 0; i < posts.length; i++) {
+              forEachFollowerPostDo(follower, posts[i]);
+            }
+          } else {
+            console.error("no posts found for " + follower.id)
+          }
+        });
+      }
+    });
+
+  }
+
+  hightimes.forAllPostsOfFollowersDo = forAllPostsOfFollowersDo;
 }).call(this);
